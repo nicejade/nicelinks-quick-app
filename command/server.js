45a4f979540efde528a41e0a61a3108f6a1178e2
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-01-05 01:18:14 +0800

    :zap: update hap-toolkit to 0.1.1

--- command/server.js
+++ command/server.js
@@ -4,33 +4,43 @@ const utils = require('./utils.js')
 const shelljs = require('shelljs')
 
 /**
-  * @desc：autoOpenBrowser 开启之后，会将二维码 Server 地址，在浏览器自动打开；
-  * @date: 2018-09-01
-  */
+ * @desc：autoOpenBrowser 开启之后，会将二维码 Server 地址，在浏览器自动打开；
+ * @date: 2018-09-01
+ */
 const autoOpenBrowser = true
 
 const startServer = () => {
   portfinder.basePort = +process.env.PORT || 8080
 
-  portfinder.getPortPromise().then(port => {
-    const urls = utils.prepareUrls('http', '0.0.0.0', port)
+  portfinder
+    .getPortPromise()
+    .then(port => {
+      const urls = utils.prepareUrls('http', '0.0.0.0', port)
 
-    shelljs.exec(`hap server --port ${port}`, { async: true })
-    autoOpenBrowser && utils.startBrowserProcess(urls.lanUrlForBrowser)
+      const child = shelljs.exec(`hap server --port ${port}`, { async: true })
+      let isBrowserOpened = false
+      child.stdout.on('data', () => {
+        // 在 hap-toolkit 编译完成后，再自动打开浏览器二维码展示 @2019-01-02；
+        autoOpenBrowser && !isBrowserOpened && utils.startBrowserProcess(urls.lanUrlForBrowser)
+        isBrowserOpened = true
+      })
 
-    printInfoAtTheTerminal(urls)
-  }).catch(error => {
-    console.log(`${chalk.red('✘')} Opps, Something Error：\n`, error)
-  })
+      printInfoAtTheTerminal(urls)
+    })
+    .catch(error => {
+      console.log(`${chalk.red('✘')} Opps, Something Error：\n`, error)
+    })
 }
 
-const printInfoAtTheTerminal = (urls) => {
+const printInfoAtTheTerminal = urls => {
   console.log()
-  console.log([
-    `App running at:`,
-    `   ${chalk.green('✔')} Local:  ${chalk.cyan(urls.localUrlForTerminal)}`,
-    `   ${chalk.green('✔')} Online: ${chalk.cyan(urls.lanUrlForTerminal)}`
-  ].join('\n'))
+  console.log(
+    [
+      `App running at:`,
+      `   ${chalk.green('✔')} Local:  ${chalk.cyan(urls.localUrlForTerminal)}`,
+      `   ${chalk.green('✔')} Online: ${chalk.cyan(urls.lanUrlForTerminal)}`
+    ].join('\n')
+  )
   console.log()
 }
 
